c874fa06123d7db0bc112e5a66cb4acf05eabd91
Matěj Sychra <igraczech@igraczech.com>
2017-01-07 22:27:07 +0100

    initial working version

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
  * This HAP device connects to defined or default mqtt broker/channel and creates a temperature service(s).
  */
 
+
 var Service, Characteristic;
 
 // should go from config
 var default_broker_address = 'mqtt://localhost'
 var default_mqtt_channel = "/sht/2"
 
+'use strict';
+
+var querystring = require('querystring');
+var http = require('http');
 var mqtt = require('mqtt')
+
 var mqttClient = null; // will be non-null if working
 
 module.exports = function(homebridge) {
@@ -18,75 +24,130 @@ module.exports = function(homebridge) {
 }
 
 function TempSensor(log, config) {
-    this.log = log;
+  this.log = log;
 
-    this.name = config['name'] || "DHT Sensor";
-    this.mqttBroker = config['mqtt_broker'];
-    this.mqttChannel = config['mqtt_channel'];
-    this.shortIdentifier = config['device_identifier'];    
+  this.name = config['name'] || "DHT Sensor";
+  this.mqttBroker = config['mqtt_broker'];
+  this.mqttChannel = config['mqtt_channel'];
+  this.shortIdentifier = config['device_identifier'];   
+  this.temperature = 0;
+  this.humidity = 0; 
+  
+  this.temperatureService = new Service.TemperatureSensor(this.name, "temperature")
+  this.temperatureService
+    .getCharacteristic(Characteristic.CurrentTemperature)
+    .on('get', this.getTemperature.bind(this))
 
-    if (!this.mqttBroker) {
-        this.log.warn('Config is missing mqtt_broker, fallback to default.');        
-        this.mqttBroker = default_broker_address;
-        if (!this.mqttBroker.contains("mqtt://")) {
-            this.mqttBroker = "mqtt://" + this.mqttBroker;
-        }
-    }
-
-    if (!this.mqttChannel) {
-        this.log.warn('Config is missing mqtt_channel, fallback to default.');
-        this.mqttChannel = default_mqtt_channel;        
-    }
-
-    init_mqtt(this.mqttBroker, this.mqttChannel);
-}
-
-function init_mqtt(broker_address, channel) {
-  console.log("Connecting to mqtt broker: " + broker_address)
-  mqttClient = mqtt.connect(broker_address)
+  this.humidityService = new Service.HumiditySensor(this.name, "humidity")
+  this.humidityService
+    .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+    .on('get', this.getHumidity.bind(this))
 
   var that = this
 
-  mqttClient.on('connect', function () {
-    console.log("MQTT connected, subscribing to: " + channel)
-    mqttClient.subscribe(channel + "/status")
-  })
+  this.getServices();
 
-  mqttClient.on('error', function () {
-    console.log("MQTT connected, subscribing to: " + channel)
-    mqttClient.subscribe(channel + "/status")
-  })
-
-  mqttClient.on('offline', function () {
-    console.log("MQTT connected, subscribing to: " + channel)
-    mqttClient.subscribe(channel + "/status")
-  })
-
-  mqttClient.on('message', function (topic, message) {
-    console.log("message: " + message.toString())
-
-    var pin = 0
-
-    if (topic == channel) {            
-      if (this.shortIdentifier == message.shortIdentifier) {
-
-        this.temperature = parseFloat(message.temperature)
-        this.humidity = parseFloat(message.humidity)
-
-        this.temperatureService
-        .getCharacteristic(Characteristic.CurrentTemperature)
-        .setValue(this.temperature);
-
-        this.humidityService
-        .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-        setValue(this.humidity)
-
-        console.log("[processing] " + mqtt_channel + " to " + message)
+  if (!this.mqttBroker) {
+      this.log.warn('Config is missing mqtt_broker, fallback to default.');        
+      this.mqttBroker = default_broker_address;
+      if (!this.mqttBroker.contains("mqtt://")) {
+          this.mqttBroker = "mqtt://" + this.mqttBroker;
       }
-    }
+  }
 
-  })
-}
+  if (!this.mqttChannel) {
+      this.log.warn('Config is missing mqtt_channel, fallback to default.');
+      this.mqttChannel = default_mqtt_channel;        
+  }
+
+  init_mqtt(this.mqttBroker, this.mqttChannel, this.temperatureService, this.humidityService);
+
+  /* Sends a JSON message to Elasticsearch database */
+  function elk(json_message)
+  {
+    var http = require('http');
+
+    var options = {
+      host: 'mini.local',
+      port: '9200',
+      path: '/telemetry-1/status',
+      method: 'POST'
+    };
+
+    function callback(response) {
+      var str = ''
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      response.on('end', function () {
+        console.log(str);
+      });
+    };
+
+    var elk = http.request(options, callback);
+    var data = JSON.stringify(json_message)
+    elk.write(data);
+    elk.end();
+  }
+
+  function init_mqtt(broker_address, channel, ts, hs) {
+    that.log("Connecting to mqtt broker: " + broker_address + " channel: "+channel)
+    mqttClient = mqtt.connect(broker_address)
+
+    //var that = this
+
+    mqttClient.on('connect', function () {
+      that.log("MQTT connected, subscribing to: " + channel)
+      mqttClient.subscribe(channel)
+    })
+
+    mqttClient.on('error', function () {
+      that.log("MQTT connected, subscribing to: " + channel)
+      mqttClient.subscribe(channel)
+    })
+
+    mqttClient.on('offline', function () {
+      that.log("MQTT connected, subscribing to: " + channel)
+      mqttClient.subscribe(channel)
+    })  
+
+    mqttClient.on('message', function (topic, message) {
+      that.log("message: " + message.toString())
+      
+      if (topic == channel) {
+
+        if (this.shortIdentifier == message.shortIdentifier) {
+
+          var m = JSON.parse(message)
+
+          var t = m.temperature;
+          var h = m.humidity;
+
+          that.temperature = t;
+          this.temperature = t;
+
+          that.humidity = h;
+          this.humidity = h;
+
+          ts
+          .getCharacteristic(Characteristic.CurrentTemperature)
+          .setValue(this.temperature);
+
+          hs
+          .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+          .setValue(this.humidity)
+
+          console.log("[processing] " + channel + " to " + message)
+
+          elk(message)
+        } 
+      }
+
+    })
+  }
+
+} // end class
 
 TempSensor.prototype.getTemperature = function(callback) {
     this.log('getTemperature callback(null, '+this.temperature+')');
@@ -107,19 +168,9 @@ TempSensor.prototype.getServices = function() {
       .setCharacteristic(Characteristic.Model, "Temperature Sensor")
       .setCharacteristic(Characteristic.SerialNumber, "3");
 
-    var temperatureService = new Service.TemperatureSensor(this.name, "temperature")
-        temperatureService
-        .getCharacteristic(Characteristic.CurrentTemperature)
-        .on('get', this.getTemperature.bind(this))
-
-    var humidityService = new Service.HumiditySensor(this.name, "humidity")
-        humidityService
-        .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-        .on('get', this.getHumidity.bind(this))
-
-    this.humidityService = humidityService
-
-    this.temperatureService = temperatureService
-
-    return [temperatureService, humidityService, informationService];
+    return [this.temperatureService, this.humidityService, informationService];
 }
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});
